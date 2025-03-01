b00732e75043d8afb3b9c924ed128177120253ff
piku69999 <capiku458@gmail.com>
2025-03-01 19:16:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token : "MTM0NTI2NzczMTA5NTc1Mjc0Ng.GvluhI.Y_uNGFl9mvUlXkZJFB5-F-y6jLUTvLDGSJ4Zuc",
+    token : "",
     mongoURI : "mongodb+srv://dakuu9838:l4ITXyBWsySRL0Hg@cluster0.5ttq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
